23421c28d014c66b2fd0d8301bccb294db111c33
Tijogomez <tijogomez555@gmail.com>
2022-03-25 10:39:50 +0530

    Submission

---
+++ pokedex/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:pokedex/app_navigator.dart';
+import 'package:pokedex/bloc/nav_cubit.dart';
+import 'package:pokedex/bloc/pokemon_bloc.dart';
+import 'package:pokedex/bloc/pokemon_details_cubit.dart';
+import 'package:pokedex/bloc/pokemon_event.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final pokemonDetailsCubit = PokemonDetailsCubit();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).copyWith(primaryColor: Colors.red),
+      home: MultiBlocProvider(providers: [
+        BlocProvider(
+            create: (context) =>
+                PokemonBloc()..add(PokemonPageRequest(page: 0))),
+        BlocProvider(
+            create: (context) =>
+                NavCubit(pokemonDetailsCubit: pokemonDetailsCubit)),
+        BlocProvider(create: (context) => pokemonDetailsCubit)
+      ], child: AppNavigator()),
+    );
+  }
+}
